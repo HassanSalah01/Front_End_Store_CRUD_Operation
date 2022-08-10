0848b86f92969f26e61e0575a150a3c3eb239a0c
HassanSalah01 <hassan.salah.eldine1@gmail.com>
2022-08-10 22:04:21 +0200

    Updated_App.js

--- app.js
+++ app.js
@@ -1,48 +1,59 @@
-const getElement = (id)=>{
+const getElement = (id) => {
     return document.getElementById(id);
-}
+};
 
 const title = getElement("title");
-const count  = getElement("count");
+const count = getElement("count");
 const discount = getElement("discount");
 const tax = getElement("tax");
 const price = getElement("price");
 const category = getElement("category");
-const elements = [title,count,price,discount,tax,category];
+const elements = [title, count, price, discount, tax, category];
 const create = getElement("btn");
 const total = getElement("total");
-
-const eve = ()=>{
-    for (let i of elements){
-        i.addEventListener("click",()=>{
-            i.value = "";
-        })
+let num = 0 ;
+btn.addEventListener("click",()=>{
+    if(validateForm()){
+        const arr = []
+        const ob = {}
+        for(let i of elements){
+            const name = i.id;
+            const value = i.value;
+            ob[name] = value;
+        }
+        console.log(ob);
+        num++;
+        localStorage.setItem(`Product   ${num}`,ob);
+    }else{
+        console.log("Missing Fields")
     }
-}
-eve();
-validation("title");
-create.addEventListener("click",()=>{
-    console.log(title.value);
-    console.log("testttttttttttt",parseInt(title.value));
-    console.log(validation("title"));
 })
-setInterval(()=>{
-    if(validation("price") && validation("tax") && validation("discount")){
-        const totales = parseFloat(price.value)+parseFloat(tax.value)+parseFloat(discount.value);
-        console.log(totales);
-        total.value = totales
-    }
-},1000)
-const main=()=>{
-    
+
+const main = ()=>{
+    clearTab();
+    totaling();
 }
+main();
+
+
+// setInterval(() => {
+//     if (validation("price") && validation("tax") && validation("discount")) {
+//         const totales =
+//             parseFloat(price.value) +
+//             parseFloat(tax.value) +
+//             parseFloat(discount.value);
+//         console.log(totales);
+//         total.value = totales;
+//     }
+// }, 1000);
+// const main = () => {};
 // const createProduct=()=>{
 
 // }
 
 // const x = {
 //     title:"iphone",
-    
+
 //         price:1100,
 //         tax:14,
 //         discount:10,
