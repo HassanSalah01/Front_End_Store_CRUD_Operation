630652a157c5b51d333aac0b1ad7b16123431e21
HassanSalah01 <hassan.salah.eldine1@gmail.com>
2022-08-14 15:25:03 +0200

    updated_app.js

--- app.js
+++ app.js
@@ -1,63 +1,53 @@
-const getElement = (id) => {
-    return document.getElementById(id);
-};
-
-const title = getElement("title");
+const model = getElement("model");
+const brand = getElement("brand")
 const count = getElement("count");
 const discount = getElement("discount");
 const tax = getElement("tax");
 const price = getElement("price");
 const category = getElement("category");
-const elements = [title, count, price, discount, tax, category];
-const create = getElement("btn");
 const total = getElement("total");
-let num = 0 ;
+const elements = [model,brand, count, price, discount, tax, category, total];
+const create = getElement("btn");
+const del = getElement("co");
+console.log(del);
+let arr = [];
+let productsArray = []
 
-btn.addEventListener("click",()=>{
-    if(validateForm()){
-        const arr = []
-        const ob = {}
-        for(let i of elements){
-            const name = i.id;
-            const value = i.value;
-            ob[name] = value;
-        }
-        console.log(ob);
-        num++;
-        localStorage.setItem(`Product   ${num}`,ob);
-    }else{
-        console.log("Missing Fields")
-    }
+del.addEventListener(("click"),()=>{
+    localStorage.clear();
 })
 
-const main = ()=>{
+btn.addEventListener("click", () => {
+    if (validateForm()) {
+        const newProd = {
+            model: model.value,
+            brand:brand.value,
+            tax: tax.value,
+            price: price.value,
+            discount: discount.value,
+            count: count.value,
+            category: category.value,
+            total: total.value,
+        };
+        const prod = new Product(newProd);
+        localStorage.setItem(`${newProd.model}`,JSON.stringify(prod));
+        cElement(prod)
+        refrechTab();
+
+    } else {
+        alert("Missing Fields");
+    }
+});
+
+const main = () => {
     clearTab();
     totaling();
-}
+    const s = JSON.parse(localStorage.getItem("s23"));
+    console.log(Object.keys(localStorage))
+    prodArr();
+    createTable(productsArray);
+
+    
+};
+
 main();
-
-
-// setInterval(() => {
-//     if (validation("price") && validation("tax") && validation("discount")) {
-//         const totales =
-//             parseFloat(price.value) +
-//             parseFloat(tax.value) +
-//             parseFloat(discount.value);
-//         console.log(totales);
-//         total.value = totales;
-//     }
-// }, 1000);
-// const main = () => {};
-// const createProduct=()=>{
-
-// }
-
-// const x = {
-//     title:"iphone",
-
-//         price:1100,
-//         tax:14,
-//         discount:10,
-//         count :3,
-//         category:"Phone"
-// }
