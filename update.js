b2af15314fcb22dc77e463753f84e1a558bb8199
HassanSalah01 <hassan.salah.eldine1@gmail.com>
2022-08-21 19:43:14 +0200

    Added_Update

---
+++ update.js
@@ -0,0 +1,12 @@
+const update = (x)=>{
+    updatedElement = [x.model]
+    btn.value = "Update"
+    brand.value = x.brand;
+    model.value = x.model;
+    price.value = x.price;
+    tax.value = x.tax;
+    category.value = x.category;
+    discount.value = x.discount;
+    total.value = x.total;
+    count.value = x.count;
+}
