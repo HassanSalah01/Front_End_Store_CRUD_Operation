5b65b507c82fd23a7dd9b22041a8fa49de6a7ff9
HassanSalah01 <hassan.salah.eldine1@gmail.com>
2022-08-21 19:42:36 +0200

    Added_Element

---
+++ Elements.js
@@ -0,0 +1,30 @@
+const model = getElement("model");
+const brand = getElement("brand");
+const count = getElement("count");
+const discount = getElement("discount");
+const tax = getElement("tax");
+const price = getElement("price");
+const category = getElement("category");
+const total = getElement("total");
+const search = getElement("search");
+const sBrand = getElement("sBrand");
+const sModel = getElement("sModel");
+const sCategory = getElement("sCategory");
+const create = getElement("btn");
+const del = getElement("co");
+const showAll = getElement("showAll");
+const elements = [
+    model,
+    brand,
+    count,
+    price,
+    discount,
+    tax,
+    category,
+    total,
+    search,
+];
+console.log(del);
+let arr = [];
+let productsArray = [];
+let updatedElement = null;
