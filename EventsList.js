365440e7541cd4c6851c42ead8220a54a4ad65ab
HassanSalah01 <hassan.salah.eldine1@gmail.com>
2022-08-22 09:37:45 +0200

    Added_prettier_And_Event_list

---
+++ EventsList.js
@@ -0,0 +1,92 @@
+del.addEventListener("click", () => {
+    localStorage.clear();
+});
+
+btn.addEventListener("click", () => {
+    if (validateForm()) {
+        const newProd = {
+            model: model.value,
+            brand: brand.value,
+            tax: tax.value,
+            price: price.value,
+            discount: discount.value,
+            count: count.value,
+            category: category.value,
+            total: total.value,
+        };
+        const prod = new Product(newProd);
+        const testing = prod.model;
+        console.log(productsArray)
+        if(btn.value =="Update"){
+            console.log("update enter")
+            localStorage.removeItem(updatedElement);
+            localStorage.setItem(`${newProd.model}`, JSON.stringify(prod));
+            // cElement(prod,"four");
+            refrechTab();
+            const element = document.getElementById("four")
+            Product.num = 0
+            while (element.firstChild) {
+                element.firstChild.remove();
+            }
+            productsArray = [];
+            prodArr();
+            createTable(productsArray,"four")
+        }else{
+            localStorage.setItem(`${newProd.model}`, JSON.stringify(prod));
+            cElement(prod, "four");
+            refrechTab();
+
+        }
+        // const prod = new Product(newProd);
+    } else {
+        alert("Missing Fields");
+    }
+});
+showAll.addEventListener("click", () => {
+    document.getElementById("five").style.display = "none";
+    document.getElementById("four").style.display = "block";
+});
+window.addEventListener("keydown",(event)=>{
+    console.log(event.key)
+    if(event.key=="Enter"){
+        if (validateForm()) {
+        const newProd = {
+            model: model.value,
+            brand: brand.value,
+            tax: tax.value,
+            price: price.value,
+            discount: discount.value,
+            count: count.value,
+            category: category.value,
+            total: total.value,
+        };
+        const prod = new Product(newProd);
+        const testing = prod.model;
+        console.log(productsArray)
+        if(btn.value =="Update"){
+            console.log("update enter")
+            localStorage.removeItem(updatedElement);
+            localStorage.setItem(`${newProd.model}`, JSON.stringify(prod));
+            // cElement(prod,"four");
+            refrechTab();
+            const element = document.getElementById("four")
+            Product.num = 0
+            while (element.firstChild) {
+                element.firstChild.remove();
+            }
+            productsArray = [];
+            prodArr();
+            createTable(productsArray,"four")
+        }else{
+            localStorage.setItem(`${newProd.model}`, JSON.stringify(prod));
+            cElement(prod, "four");
+            refrechTab();
+
+        }
+        // const prod = new Product(newProd);
+    } else {
+        alert("Missing Fields");
+    }
+
+    }
+})
